1b08f05c623eae5fb46c9d5112769078c9f6d2b6
Nylon <diego.gomes87@gmail.com>
2015-11-07 11:44:05 -0200

    Photo - layout,views controllers e frameworks

---
+++ Photo/IPhoto.swift
@@ -0,0 +1,18 @@
+//
+//  IPhoto.swift
+//  Photo
+//
+//  Created by Diego Gomes on 06/11/2015.
+//  Copyright (c) 2015 Nylon. All rights reserved.
+//
+
+import Foundation
+
+struct IPhoto{
+    
+    var name: String
+    var fileName: String
+    var notes: String
+    
+    
+}
